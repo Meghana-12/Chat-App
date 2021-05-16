fc995bca2dc680e676c6a213525d5e8903bb5e1f
Meghana-12 <meghana.cosmos@gmail.com>
2021-05-16 13:33:08 +0530

    sendMessage working

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -5,135 +5,171 @@ import { auth, db } from "../firebase";
 import { Message } from "./Message";
 import { Avatar, IconButton } from "@material-ui/core";
 import {
-	AttachFile,
-	InsertEmoticon,
-	MoreVert,
-	Search,
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  Search,
 } from "@material-ui/icons";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { colors } from "../utils/colors";
+import { useState } from "react";
+import firebase from "firebase";
+import { getRecipientEmail } from "../utils/getRecipentEmail";
 
 export const ChatScreen = ({ chat, messages }) => {
-	const [user] = useAuthState(auth);
-	const router = useRouter();
-	const [messagesSnap] = useCollection(
-		db
-			.collection("chats")
-			.doc(router.query.id)
-			.collection("messages")
-			.orderBy("timestamp", "asc")
-	);
-	const showMessages = () => {
-		if (messagesSnap) {
-			return messagesSnap.docs.map((message) => (
-				<Message
-					key={message.id}
-					user={message.data().user}
-					message={{
-						...message.data(),
-						timestamp: message.data().timestamp?.toDate().getTime(),
-					}}
-				/>
-			));
-		}
-	};
-	return (
-		<Container>
-			<Header>
-				<UserProfile>
-					<Avatar />
-					<UserInfo>
-						<h4 style={{ margin: 0 }}>Meghana</h4>
-						<h6 style={{ margin: 0, color: "#CCC5C4" }}>seen 2 mins ago</h6>
-					</UserInfo>
-				</UserProfile>
-				<IconsContainer>
-					<IconButton>
-						<Search style={{ color: "whitesmoke" }} size="large" />
-					</IconButton>
-					<IconButton>
-						<MoreVert style={{ color: "whitesmoke" }} size="large" />
-					</IconButton>
-				</IconsContainer>
-			</Header>
-			<MessageContainer>
-				{showMessages()}
-				<Message />
-				<Message />
-				<Message />
-				<EndOfMessage />
-			</MessageContainer>
-			<InputContainer>
-				<IconButton>
-					<InsertEmoticon style={{ color: "whitesmoke" }} size="large" />
-				</IconButton>
-				<IconButton>
-					<AttachFile style={{ color: "whitesmoke" }} size="large" />
-				</IconButton>
-				<StyledInput />
-			</InputContainer>
-		</Container>
-	);
+  const [user] = useAuthState(auth);
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const [messagesSnap] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+  const showMessages = () => {
+    if (messagesSnap) {
+      return messagesSnap.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
+  const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("users").doc(user.uid).set(
+      {
+        lastseen: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+    db.collection("chats").doc(router.query.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      userEmail: user.email,
+      photoURL: user.photoURL,
+    });
+    setInput("");
+  };
+  // const recipientEmail = getRecipientEmail(
+  //   user,
+  //   db.collection("chats").doc(router.query.id).users()
+  // );
+  return (
+    <Container>
+      <Header>
+        <UserProfile>
+          <Avatar />
+          <UserInfo>
+            <h4 style={{ margin: 0 }}>recipientEmail</h4>
+            <h6 style={{ margin: 0, color: "#CCC5C4" }}>seen 2 mins ago</h6>
+          </UserInfo>
+        </UserProfile>
+        <IconsContainer>
+          <IconButton>
+            <Search style={{ color: "whitesmoke" }} size="large" />
+          </IconButton>
+          <IconButton>
+            <MoreVert style={{ color: "whitesmoke" }} size="large" />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <MessageContainer>
+        {/* {showMessages()} */}
+        <Message />
+        <Message />
+        <Message />
+        <EndOfMessage />
+      </MessageContainer>
+      <InputContainer>
+        <IconButton>
+          <InsertEmoticon style={{ color: "whitesmoke" }} size="large" />
+        </IconButton>
+        <IconButton>
+          <AttachFile style={{ color: "whitesmoke" }} size="large" />
+        </IconButton>
+        <StyledInput
+          placeholder="Type your message here ..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button hidden disabled={!input} type="submit" onClick={sendMessage}>
+          send message
+        </button>
+        <IconButton>
+          <Mic style={{ color: "whitesmoke" }} size="large" />
+        </IconButton>
+      </InputContainer>
+    </Container>
+  );
 };
+
 const Container = styled.div`
-	height: 100%;
-	flex: 1;
-	flex-grow: 1;
-	overflow: hidden;
+  height: 100%;
+  flex: 1;
+  flex-grow: 1;
+  overflow: hidden;
 `;
 const Header = styled.div`
-	height: 4rem;
-	border-bottom: 2px solid #161a1c;
-	background-color: #292e33;
-	width: 100%;
-	color: whitesmoke;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 2rem 1rem 2rem 2rem;
-	z-index: 100;
-	position: sticky;
-	top: 0;
+  height: 4rem;
+  border-bottom: 2px solid #161a1c;
+  background-color: #292e33;
+  width: 100%;
+  color: whitesmoke;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 1rem 2rem 2rem;
+  z-index: 100;
+  position: sticky;
+  top: 0;
 `;
 
 const UserInfo = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	height: 3rem;
-	padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 3rem;
+  padding-left: 1rem;
 `;
 
 const UserProfile = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 const IconsContainer = styled.div``;
 
 const EndOfMessage = styled.div``;
 const MessageContainer = styled.div`
-	min-height: 100%;
+  min-height: 100%;
 `;
 const InputContainer = styled.form`
-	display: flex;
-	align-items: center;
-	height: 5rem;
-	background-color: ${colors.midGray};
-	position: sticky;
-	bottom: 0px;
-	width: 100%;
-	padding: 1rem;
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  background-color: ${colors.midGray};
+  position: sticky;
+  bottom: 0px;
+  width: 100%;
+  padding: 1rem;
 
-	z-index: 100;
+  z-index: 100;
 `;
 const StyledInput = styled.input`
-	background-color: ${colors.lightGray};
-	height: 3rem;
-	border-radius: 3rem;
-	outline: none;
-	border: none;
-	flex-grow: 1;
-	flex: 1;
-	padding: 0 1rem;
-	font-size: 1.3rem;
+  background-color: ${colors.lightGray};
+  height: 3rem;
+  border-radius: 3rem;
+  outline: none;
+  border: none;
+  flex-grow: 1;
+  flex: 1;
+  padding: 0 1rem;
+  font-size: 1.3rem;
 `;
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,152 +9,154 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { Chat } from "./Chat";
 import { colors } from "../utils/colors";
+
 export const Sidebar = () => {
-	const [user] = useAuthState(auth);
-	console.log("user", user);
-	const userChats = db
-		.collection("chats")
-		.where("users", "array-contains", user.email);
-	const [chatsSnap] = useCollection(userChats);
-	console.log("hey:", chatsSnap, userChats);
-	const createChat = () => {
-		const input = prompt("Please enter the email address");
-		if (!input) return null;
-		if (
-			EmailValidator.validate(input) &&
-			input !== user.email &&
-			!duplicateCheck(input)
-		) {
-			db.collection("chats").add({ users: [user.email, input] });
-		}
-	};
-	const duplicateCheck = (recipient) => {
-		!!chatsSnap?.docs.find((chat) => {
-			chat.data().users.find((user) => user === recipient)?.length > 0;
-			// go through the chats that exist
-			// either return val or null /undefined
-			// !! : if value is truthy returns true
-			// if falsey / emoty string / undefied : returns false
-		});
-	};
+  const [user] = useAuthState(auth);
+  console.log("user", user);
+  const userChats = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatsSnap] = useCollection(userChats);
+  console.log("hey:", chatsSnap, userChats);
+  const createChat = () => {
+    const input = prompt("Please enter the email address");
+    if (!input) return null;
+    if (
+      EmailValidator.validate(input) &&
+      input !== user.email &&
+      !duplicateCheck(input)
+    ) {
+      db.collection("chats").add({ users: [user.email, input] });
+    }
+  };
+  const duplicateCheck = (recipient) => {
+    !!chatsSnap?.docs.find((chat) => {
+      chat.data().users.find((user) => user === recipient)?.length > 0;
+      // go through the chats that exist
+      // either return val or null /undefined
+      // !! : if value is truthy returns true
+      // if falsey / emoty string / undefied : returns false
+    });
+  };
 
-	return (
-		<Container>
-			<Header>
-				<UserAvatar src={user?.photoURL} />
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-					}}>
-					<h3>{user.displayName}</h3>
-					{user.email}
-				</div>
-				<IconsContainer>
-					<IconButton>
-						<ChatIcon
-							size="large"
-							style={{ color: "#eaae06" }}
-							onClick={createChat}
-						/>
-					</IconButton>
-					<IconButton>
-						<ExitToAppIcon
-							color="secondary"
-							size="large"
-							onClick={() => auth.signOut()}
-						/>
-					</IconButton>
-				</IconsContainer>
+  return (
+    <Container>
+      <Header>
+        <UserAvatar src={user?.photoURL} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>{user.displayName}</h3>
+          {user.email}
+        </div>
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon
+              size="large"
+              style={{ color: "#eaae06" }}
+              onClick={createChat}
+            />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon
+              color="secondary"
+              size="large"
+              onClick={() => auth.signOut()}
+            />
+          </IconButton>
+        </IconsContainer>
 
-				<Search>
-					<SearchIcon />
-					<SearchInput />
-					{/* add search button and on click gives searchbar (a lil animation) */}
-				</Search>
-			</Header>
-			{/* <StartButton onClick={createChat}>+ New Chat</StartButton> */}
-			{/* Chats go below */}
-			<ChatContainer>
-				{chatsSnap?.docs.map((chat) => (
-					<Chat key={chat.id} id={chat.id} users={chat.data().users} />
-				))}
-			</ChatContainer>
-			{/* add delete , pin */}
-		</Container>
-	);
+        <Search>
+          <SearchIcon />
+          <SearchInput />
+          {/* add search button and on click gives searchbar (a lil animation) */}
+        </Search>
+      </Header>
+      {/* <StartButton onClick={createChat}>+ New Chat</StartButton> */}
+      {/* Chats go below */}
+      <ChatContainer>
+        {chatsSnap?.docs.map((chat) => (
+          <Chat key={chat.id} id={chat.id} users={chat.data().users} />
+        ))}
+      </ChatContainer>
+      {/* add delete , pin */}
+    </Container>
+  );
 };
 
 export default Sidebar;
 
 const Container = styled.div`
-	width: 25rem;
-	border-right: 2px solid #161a1c;
-	height: 100vh;
-	background-color: #292e33;
+  width: 25rem;
+  border-right: 2px solid #161a1c;
+  height: 100vh;
+  background-color: #292e33;
 `;
 const Header = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	border-bottom: 1px solid #7a7c7e;
-	padding: 1rem;
-	background-color: #292e33;
-	color: #ccc5c4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #7a7c7e;
+  padding: 1rem;
+  background-color: #292e33;
+  color: #ccc5c4;
 `;
 const UserAvatar = styled(Avatar)`
-	cursor: pointer;
-	&&& {
-		height: 8rem;
-		width: 8rem;
-		border: 3px solid ${colors.black};
-	}
-	:hover {
-		opacity: 0.8;
-	}
+  cursor: pointer;
+  &&& {
+    height: 8rem;
+    width: 8rem;
+    border: 3px solid ${colors.black};
+  }
+  :hover {
+    opacity: 0.8;
+  }
 `;
 const IconsContainer = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Search = styled.div`
-	display: flex;
-	align-items: center;
-	padding: 0.5rem;
-	background-color: #161a1c;
-	border-radius: 2rem;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: #161a1c;
+  border-radius: 2rem;
 `;
 const SearchInput = styled.input`
-	outline-width: 0;
-	border: none;
-	flex: 1;
-	background-color: transparent;
-	width: 12rem;
-	color: whitesmoke;
+  outline-width: 0;
+  border: none;
+  flex: 1;
+  background-color: transparent;
+  width: 12rem;
+  color: whitesmoke;
 `;
 
 const StartButton = styled(Button)`
-	width: 100%;
-	&&& {
-		/* border-top: 1px solid gray; */
-		/* border-bottom: 1px solid gray; */
-		color: #eaae06;
-		/* background-color: #eaae06; */
-		border-radius: 0;
-	}
+  width: 100%;
+  &&& {
+    /* border-top: 1px solid gray; */
+    /* border-bottom: 1px solid gray; */
+    color: #eaae06;
+    /* background-color: #eaae06; */
+    border-radius: 0;
+  }
 `;
 /* &&& increases priority */
 
 const ChatContainer = styled.div`
-	overflow: scroll;
-	::-webkit-scrollbar {
-		display: none;
-	}
-	-ms-overflow-style: none;
-	/* IE and edge */
-	scrollbar-width: none;
-	/* Firefox */
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  /* IE and edge */
+  scrollbar-width: none;
+  /* Firefox */
 `;
